21f18c21e70801a76694a8cde1425bb6f8a0e72f
Talha Tariq <talhacoder01@gmail.com>
2022-06-22 00:52:44 +0500

    Layout Fixed | Additional Styling Added

--- script.js
+++ script.js
@@ -3,44 +3,44 @@ const buttontdl = document.querySelector('.buttoninput')
 const listtdl = document.querySelector('.todolist')
 
 function clickButton(e) {
-    e.preventDefault()
+  e.preventDefault()
 
-    const itemall = document.createElement('div')
-    itemall.classList.add('itemall')
+  const itemall = document.createElement('div')
+  itemall.classList.add('itemall')
 
-    const item = document.createElement('p')
-    item.classList.add('item')
-    item.innerText = inputtdl.value
-    itemall.appendChild(item)
+  const item = document.createElement('p')
+  item.classList.add('item')
+  item.innerText = inputtdl.value
+  itemall.appendChild(item)
 
-    if ( inputtdl.value === '' ) return
+  if (inputtdl.value === '') return
 
-    const checkbutton = document.createElement("button")
-    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>' 
-    checkbutton.classList.add("check-button")
-    itemall.appendChild(checkbutton)
+  const checkbutton = document.createElement('button')
+  checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
+  checkbutton.classList.add('check-button')
+  itemall.appendChild(checkbutton)
 
-    const trashbutton = document.createElement("button")
-    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>' 
-    trashbutton.classList.add("trash-button")
-    itemall.appendChild(trashbutton)
+  const trashbutton = document.createElement('button')
+  trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+  trashbutton.classList.add('trash-button')
+  itemall.appendChild(trashbutton)
 
-    listtdl.appendChild(itemall)
-    inputtdl.value = ''
+  listtdl.appendChild(itemall)
+  inputtdl.value = ''
 }
 
 function okdel(e) {
-    const item = e.target
+  const item = e.target
 
-    if ( item.classList[0] === 'check-button') {
-        const todolist = item.parentElement
-        todolist.classList.toggle('checklist')
-    }
+  if (item.classList[0] === 'check-button') {
+    const todolist = item.parentElement
+    todolist.classList.toggle('checklist')
+  }
 
-    if ( item.classList[0] === 'trash-button') {
-        const todolist = item.parentElement
-        todolist.remove()
-    }
+  if (item.classList[0] === 'trash-button') {
+    const todolist = item.parentElement
+    todolist.remove()
+  }
 }
 
 buttontdl.addEventListener('click', clickButton)
