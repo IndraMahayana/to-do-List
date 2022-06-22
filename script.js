b129633400f4e6ccf7f3e38fffcbe5b5d2d1b166
Talha Tariq <talhacoder01@gmail.com>
2022-06-22 21:21:16 +0500

    Optimized Code | Features Added

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 const inputtdl = document.querySelector('.input')
 const buttontdl = document.querySelector('.buttoninput')
 const listtdl = document.querySelector('.todolist')
+const tasksTagline = document.querySelector('.tagline')
 
-function clickButton(e) {
+let totalTasks = 0
+function AddTask(e) {
   e.preventDefault()
 
-  const itemall = document.createElement('div')
-  itemall.classList.add('itemall')
+  if (inputtdl.value === '') return // Guard Class
 
-  const item = document.createElement('p')
-  item.classList.add('item')
-  item.innerText = inputtdl.value
-  itemall.appendChild(item)
+  //Markup Html
+  const markUp = `
+    <div class="itemall">
+        <p class="item">${inputtdl.value}</p>
+        <button class="check-button"><i class="fa-solid fa-check"></i></button>
+        <button class="trash-button"><i class="fa-solid fa-trash"></i></button>
+    </div>
+  `
+  totalTasks++
+  if (totalTasks > 0 && tasksTagline.classList.contains('hidden'))
+    tasksTagline.classList.remove('hidden')
 
-  if (inputtdl.value === '') return
-
-  const checkbutton = document.createElement('button')
-  checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
-  checkbutton.classList.add('check-button')
-  itemall.appendChild(checkbutton)
-
-  const trashbutton = document.createElement('button')
-  trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
-  trashbutton.classList.add('trash-button')
-  itemall.appendChild(trashbutton)
-
-  listtdl.appendChild(itemall)
+  // Adding that Html to To-Do List
+  listtdl.insertAdjacentHTML('beforeend', markUp)
   inputtdl.value = ''
 }
 
-function okdel(e) {
+function MarkDone_Delete(e) {
   const item = e.target
 
   if (item.classList[0] === 'check-button') {
@@ -40,8 +37,15 @@ function okdel(e) {
   if (item.classList[0] === 'trash-button') {
     const todolist = item.parentElement
     todolist.remove()
+    totalTasks--
   }
+
+  if (totalTasks === 0 && !tasksTagline.classList.contains('hidden'))
+    tasksTagline.classList.add('hidden')
 }
 
-buttontdl.addEventListener('click', clickButton)
-listtdl.addEventListener('click', okdel)
+inputtdl.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) AddTask(e)
+})
+buttontdl.addEventListener('click', AddTask)
+listtdl.addEventListener('click', MarkDone_Delete)
